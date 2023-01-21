bad218f067cf7c06df6a478d06a2b5ddc68bd0b9
Mikal Stordal <mikalstordal@gmail.com>
2023-01-21 22:31:21 +0100

    add more comments to the hash-set converter

--- Shoko.Server/Databases/TypeConverters/StringHashSetConverter.cs
+++ Shoko.Server/Databases/TypeConverters/StringHashSetConverter.cs
@@ -21,9 +21,15 @@ public class StringHashSetConverter<T> : TypeConverter, IUserType
 {
     #region TypeConverter
 
+    /// <summary>
+    /// Returns whether this converter can convert an object of the given type to the type of this converter.
+    /// </summary>
+    /// <param name="context">An <see cref="ITypeDescriptorContext"/> that provides a format context.</param>
+    /// <param name="sourceType">A <see cref="Type"/> that represents the type you want to convert from.</param>
+    /// <returns>true if this converter can perform the conversion; otherwise, false.</returns>
     public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
     {
-        // Make sure we're trying to convert from a hash-set.
+        // Make sure we're trying to convert from a hash-set of the correct generic type.
         if (sourceType.FullName != typeof(HashSet<T>).FullName)
             return false;
 
@@ -35,11 +41,25 @@ public class StringHashSetConverter<T> : TypeConverter, IUserType
         return sourceType.GenericTypeArguments[0].Name == typeof(T).Name;
     }
 
+    /// <summary>
+    /// Returns whether this converter can convert the object to the specified type.
+    /// </summary>
+    /// <param name="context">An <see cref="ITypeDescriptorContext"/> that provides a format context.</param>
+    /// <param name="destinationType">A <see cref="Type"/> that represents the type you want to convert to.</param>
+    /// <returns>true if this converter can perform the conversion; otherwise, false.</returns>
     public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
     {
+        // Check if the destination type is string.
         return destinationType != null && destinationType.FullName == "System.String";
     }
 
+    /// <summary>
+    /// Converts the given object to the type of this converter, using the specified context and culture information.
+    /// </summary>
+    /// <param name="context">An <see cref="ITypeDescriptorContext"/> that provides a format context.</param>
+    /// <param name="culture">The <see cref="CultureInfo"/> to use as the current culture.</param>
+    /// <param name="value">The <see cref="object"/> to convert.</param>
+    /// <returns>An <see cref="object"/> that represents the converted value.</returns>
     public override HashSet<T> ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object? value)
     {
         return value switch
@@ -52,9 +72,17 @@ public class StringHashSetConverter<T> : TypeConverter, IUserType
         };
     }
 
+    /// <summary>
+    /// Converts the given value object to the specified type, using the specified context and culture information.
+    /// </summary>
+    /// <param name="context">An <see cref="ITypeDescriptorContext"/> that provides a format context.</param>
+    /// <param name="culture">A <see cref="CultureInfo"/>. If null is passed, the current culture is assumed.</param>
+    /// <param name="value">The <see cref="object"/> to convert.</param>
+    /// <param name="destinationType">The <see cref="Type"/> to convert the value to.</param>
+    /// <returns>An <see cref="object"/> that represents the converted value.</returns>
     public override string ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
     {
-        // Make sure the value is set, otherwise return an empty strig.
+        // Make sure the value is set, otherwise return an empty string.
         if (value == null)
             return "";
 
